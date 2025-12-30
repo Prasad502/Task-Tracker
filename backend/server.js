30d2562bc82c50a76cd3e05422b5ec74fe9eccfa
Prasad502 <roadstergaming001@gmail.com>
2025-12-30 22:52:17 +0530

    cors hard code removed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // when running behind a proxy (like Render) we need to trust the proxy so secure cookies work
 app.set('trust proxy', 1);
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://task-tracker-ffzs.onrender.com', credentials: true }));
 app.use(express.json());
 
 const isProd = process.env.NODE_ENV === 'production';
